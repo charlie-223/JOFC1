7d02cb6ed49f166bbf130f0cb8a0335dbd136cd7
charlie-223 <kingvacaro012@gmail.com>
2025-04-18 19:11:19 +0800

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joy of Faith North Caloocan</title>
+    <link rel="stylesheet" href="style1.css"> </head>
+    <body>
+    <nav class="main-navigation">
+            <div class="container">
+        <header class="site-header">
+            <div class="logo">
+                <ul>
+                    <li> <img src="./img/ico/logo.png" alt=""> </li>
+                    <li>  
+                        <div class="logo-text">
+                       <h2>   Joy of Faith<br>North Caloocan </h2>
+                    </div>
+                </li>
+                </ul>
+            </div>
+
+            <ul>
+                    <li style="font-size: 20px;"><a href="index.php">Home</a></li>
+                    <li style="font-size: 20px;"><a href="events.php">Events</a></li>
+                    <li style="font-size: 20px;"><a href="history.php">History</a></li>
+                    <li style="font-size: 20px;"><a href="blog.php" class="active">Blog / News</a></li>
+                    <li style="font-size: 20px;"><a href="about.php">About Us</a></li>
+                </ul>
+                <div class="logbot">
+              <a href="signup.php"><img src="./img/ico/profile.png" style=" height: 50px;
+    width: 50px;" alt=""> </a>
+                </div>
+            </nav>
+            <div class="header-actions">
+                </div>
+        </header>
+
+        <footer class="site-footer">
+            <p>JOY OF FAITH CHURCH<br>
+            BAGUMBONG, NORTH CALOOCAN<br>
+            Metro Manila, Philippines</p> <p>&copy; <?php echo date("Y"); ?> Joy of Faith North Caloocan. All rights reserved.</p> </footer>
+
+        </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joy of Faith North Caloocan</title>
+    <link rel="stylesheet" href="style1.css"> </head>
+    <body>
+    <nav class="main-navigation">
+            <div class="container">
+        <header class="site-header">
+            <div class="logo">
+                <ul>
+                    <li> <img src="./img/ico/logo.png" alt=""> </li>
+                    <li>  
+                        <div class="logo-text">
+                       <h2>   Joy of Faith<br>North Caloocan </h2>
+                    </div>
+                </li>
+                </ul>
+            </div>
+            
+                <ul>
+                    
+                    <li style="font-size: 20px;"><a href="index.php" class="active">Home</a></li>
+                    <li style="font-size: 20px;"><a href="events.php">Events</a></li>
+                    <li style="font-size: 20px;"><a href="history.php">History</a></li>
+                    <li style="font-size: 20px;"><a href="blog.php">Blog / News</a></li>
+                    <li style="font-size: 20px;"><a href="about.php">About Us</a></li>
+                </ul>
+
+                <div>
+
+                </div>
+                <div class="logbot">
+              <a href="signup.php"><img src="./img/ico/profile.png" style=" height: 50px;
+    width: 50px;" alt=""> </a>
+                </div>
+            </nav>
+            <div class="header-actions">
+                </div>
+        </header>
+     
+        <section class="hero-section">
+            <div class="hero-content ">
+                <h1 class="hero-title" style=" font-size: 60px;">One Heart, One Goal to win souls and to make disciples, According to the calling of Christ-Jesus.</h1>
+                <div class="hero-buttons">
+                    <a href="donate.php" class="button primary">DONATE</a>
+                    <a href="signup.php" class="button secondary">VOLUNTEER</a>
+                </div>
+                <br>
+                <br>
+                <br>
+                <div class="MV">
+                  <ul>
+                    <li><img src="./img/ico/target.png"  alt=""></li>
+                    <li>
+                    <h1>MISSION</h1>    
+                    <p>To Win Souls and Make Disciples</p>
+                 </li>
+                 <br>
+                 <br>
+                 <li><img src="./img/ico/view.png" alt=""></li>
+                    <li>
+                      <h1>VISION</h1>  
+                        <p>To Make Every Believer a Leader</p>
+                    </li>
+                  </ul>
+                </div>
+                </div>
+                <div class="hero-image">
+                <img src="jofc.jpeg">
+            </div>
+            </section>
+        </div>
+</body>
+</html>
